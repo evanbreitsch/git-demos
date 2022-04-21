2c3e009e5ccd3d74c9d63258dcf6f13822ef5006
Evan Breitsch <evan.breitsch@gmail.com>
2022-04-21 12:51:59 -0600

    rename tellCompliment -> getCompliment

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const say = (message) => {
   console.log(message);
 }
 
-const tellCompliment = () => {
+const getCompliment = () => {
   return `I like the way your code smells`;
 }
 
 (function main () {
   say(`Hello I'm a duck!`);
+  say(getCompliment());
 })();
