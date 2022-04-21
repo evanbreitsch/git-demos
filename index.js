b90f72d28b5cfc480eaf572b2208a638d68fad26
Evan Breitsch <evan.breitsch@gmail.com>
2022-04-21 12:49:32 -0600

    feature: get joke

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const say = (message) => {
   console.log(message);
 }
 
+const getJoke = () => {
+  return 'undefined';
+}
+
 (function main () {
   say(`Hello I'm a duck!`);
+  say(getJoke());
 })();
