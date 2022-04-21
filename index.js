7b381b0f0554689f9068abc9c6b12ad9615da1a5
Evan Breitsch <evan.breitsch@gmail.com>
2022-04-21 12:40:39 -0600

    feature: tell compliment

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const getJoke = () => {
   return 'undefined';
 }
 
+const tellCompliment = message => {
+  return `I like the way your code smells`;
+}
+
 (function main () {
   say(`Hello I'm a duck!`);
   say(getJoke());
